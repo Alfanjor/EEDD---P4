f09855af2ec96c93a1f2d1bc13dd08c90a9188f8
Alfanjor <xalfx.gm@gmail.com>
2014-11-20 13:37:23 +0100

    Versión estable

---
+++ build/Debug/GNU-Linux-x86/main.o.d
@@ -0,0 +1,5 @@
+build/Debug/GNU-Linux-x86/main.o: main.cpp tinythread.h millisleep.h
+
+tinythread.h:
+
+millisleep.h:
---
+++ build/Debug/GNU-Linux-x86/tinythread.o.d
@@ -0,0 +1,3 @@
+build/Debug/GNU-Linux-x86/tinythread.o: tinythread.cpp tinythread.h
+
+tinythread.h:
